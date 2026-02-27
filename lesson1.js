6ad43a373e45b2be459624391b03f76ccb896066
Umid2720 <umidbekruzmetov309@gmail.com>
2026-02-27 16:06:32 +0500

    we has been learnt variable types(let, var, const)

--- lesson1.js
+++ lesson1.js
@@ -1,5 +1,20 @@
+// let
 let x = 5
 let y = 10
 
 let sum = x + y
 console.log("The sum of x and y is: " + sum)
+
+// var
+var a = 7
+var b = 3
+
+var difference = a - b
+console.log("The difference of x and y is: " + difference)
+
+// const
+const pi = 3.14
+const radius = 5
+
+const area = pi * radius * radius
+console.log("The area of the circle is: " + area)
