c2798701de3cb65a7a5b2ee9385f294dff92d448
Umid2720 <umidbekruzmetov309@gmail.com>
2026-02-27 16:09:48 +0500

    We has been learnt operators

---
+++ lesson2.js
@@ -0,0 +1,19 @@
+// operators/ arithmetic operators
+let a = 10;
+let b = 5;
+
+let sum = a + b; // addition
+console.log("The sum of a and b is: " + sum);
+
+let difference = a - b; // subtraction
+console.log("The difference of a and b is: " + difference);
+
+let product = a * b; // multiplication
+console.log("The product of a and b is: " + product);   
+
+let quotient = a / b; // division
+console.log("The quotient of a and b is: " + quotient); 
+
+let remainder = a % b; // modulus
+console.log("The remainder of a divided by b is: " + remainder);
+
